62729eb0f6d1adb7c52d33e448875e21f24a466c
Kieren Davies <kieren@kierendavies.co.za>
2012-09-08 23:53:41 +0200

    skeleton app with layout

---
+++ src/za/co/kierendavies/tempotapper/TempoTapper.java
@@ -0,0 +1,15 @@
+package za.co.kierendavies.tempotapper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TempoTapper extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
