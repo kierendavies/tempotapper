f5380119b63463c0f6473b9ea94da1175ae10ee7
Kieren Davies <kieren@kierendavies.co.za>
2012-09-09 14:23:03 +0200

    fix #1

--- src/za/co/kierendavies/tempotapper/TempoTapper.java
+++ src/za/co/kierendavies/tempotapper/TempoTapper.java
@@ -3,13 +3,14 @@ package za.co.kierendavies.tempotapper;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
 public class TempoTapper extends Activity {
     private double minuteMillis = 60000;
     private String bpmSuffix;
-    private int taps = -1;
+    private int taps = 0;
     private long startTime;
     private TextView bpmText;
 
@@ -18,11 +19,27 @@ public class TempoTapper extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        bpmText = (TextView) findViewById(R.id.bpm);
+        bpmText = (TextView) findViewById(R.id.text_bpm);
         bpmSuffix = getResources().getString(R.string.bpm_suffix);
+
+        findViewById(R.id.button_tap).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    tap();
+                    view.setPressed(true);
+                    return true;
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    view.setPressed(false);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
     }
 
-    public void tap(View view) {
+    public void tap() {
         double bpm = 0;
         if (taps == 0) {
             startTime = SystemClock.uptimeMillis();
@@ -31,6 +48,10 @@ public class TempoTapper extends Activity {
             bpmText.setText(String.format("%.1f", bpm) + bpmSuffix);
         }
         taps++;
+    }
+
+    public void tap(View view) {
+        tap();
     }
 
     public void reset(View view) {
