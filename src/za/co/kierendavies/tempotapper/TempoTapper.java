25da359d764bb2c598ad06b10ec66a203cd5b746
Kieren Davies <kieren@kierendavies.co.za>
2013-04-05 13:32:59 +0300

    add prompt to rate

--- src/za/co/kierendavies/tempotapper/TempoTapper.java
+++ src/za/co/kierendavies/tempotapper/TempoTapper.java
@@ -2,13 +2,19 @@ package za.co.kierendavies.tempotapper;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TempoTapper extends Activity {
     private double minuteMillis = 60000;
@@ -17,6 +23,8 @@ public class TempoTapper extends Activity {
     private long startTime;
     private long lastTime;
     private TextView bpmText;
+    private SharedPreferences sharedPref;
+    private int timesLaunched;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,7 +32,7 @@ public class TempoTapper extends Activity {
         setContentView(R.layout.main);
 
         bpmText = (TextView) findViewById(R.id.text_bpm);
-        bpmSuffix = getResources().getString(R.string.bpm_suffix);
+        bpmSuffix = getString(R.string.bpm_suffix);
 
         findViewById(R.id.button_tap).setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -41,6 +49,15 @@ public class TempoTapper extends Activity {
                 }
             }
         });
+
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        timesLaunched = sharedPref.getInt("timesLaunched", 0);
+        if (timesLaunched != -1) {
+            ++timesLaunched;
+            sharedPref.edit()
+                    .putInt("timesLaunched", timesLaunched)
+                    .commit();
+        }
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -50,7 +67,51 @@ public class TempoTapper extends Activity {
 
     public void onBackPressed() {
         if (taps == 0) {
-            super.onBackPressed();
+            if (timesLaunched >= 5) {
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.rate_title)
+                        .setMessage(R.string.rate_message)
+                        .setPositiveButton(R.string.rate_yes,
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        sharedPref.edit()
+                                                .putInt("timesLaunched", -1)
+                                                .commit();
+                                        startActivity(new Intent(Intent.ACTION_VIEW,
+                                                Uri.parse("market://details?id=" + getPackageName())));
+                                        finish();
+                                    }
+                                })
+                        .setNeutralButton(R.string.rate_later,  // ask next time
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+                                    }
+                                })
+                        .setNegativeButton(R.string.rate_no,  // don't ask again
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        sharedPref.edit()
+                                                .putInt("timesLaunched", -1)
+                                                .commit();
+                                        finish();
+                                    }
+                                })
+                        .setOnCancelListener(
+                                new DialogInterface.OnCancelListener() {
+                                    @Override
+                                    public void onCancel(DialogInterface dialog) {
+                                        finish();
+                                    }
+                                })
+                        .create()
+                        .show();
+            } else {
+                finish();
+            }
         } else {
             reset();
         }
